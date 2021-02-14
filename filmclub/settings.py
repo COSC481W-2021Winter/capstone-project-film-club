7484d6292056381ccc5715637f65d3470847a280
Nathan Fishel <n_fishel-v@dcu.org>
2021-02-14 14:22:50 -0500

    static files working

--- filmclub/settings.py
+++ filmclub/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'core/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 ##### Authentication stuff
 #SITE_ID = 1
