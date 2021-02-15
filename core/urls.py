7d41d001b24fef2452d17c9507338f09a0db5523
Nathan Fishel <n_fishel-v@dcu.org>
2021-02-15 16:25:38 -0500

    bug fixes

--- core/urls.py
+++ core/urls.py
@@ -43,7 +43,7 @@ urlpatterns = [
     ),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view( template_name='registration/password-reset/password_reset_done.html'),name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password-reset/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password-reset/password_reset_complete.html'), name='password_reset_complete'),
+    path('password-reset-complete//<uidb64>/<token>/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password-reset/password_reset_complete.html'), name='password_reset_complete'),
     path("welcome/", views.welcome, name = "send welcome email"),
 
     path('watch/', views.watch, name = 'watch'),
