4b932c934b3ea0ebfded653035faafc6706e1e36
Nathan Fishel <n_fishel-v@dcu.org>
2021-02-20 15:09:33 -0500

    wrote sample test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from core.models import *
 # Create your tests here.
+
+class GenreTestCase(TestCase):
+    def setUp(self):
+        Genre.objects.create(name="CO", api_id=3)
+
+    def test_genre_name(self):
+        test = Genre.objects.get(api_id=3)
+        self.assertEqual(test.__str__(), 'CO')
