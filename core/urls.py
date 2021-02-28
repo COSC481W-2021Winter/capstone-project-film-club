ff87e9b262d5428be91d6d94eba1a26fc3632bca
Nathan Fishel <n_fishel-v@dcu.org>
2021-02-28 15:07:42 -0500

    fixed password reset

--- core/urls.py
+++ core/urls.py
@@ -21,36 +21,21 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', views.home, name = 'home'),  # Home/landing screen
-    path('accounts/', include('django.contrib.auth.urls')),  # Enable Django auth app
     path('register/', views.register, name='register'),  # New user signup screen
     path('genres/', views.genres, name = 'genres'),  # Genre selection screen
     path('search', views.search, name = 'search'),  # Search screen
     path('m/add', views.add_movie, name = 'add_movie'),  # Add movie profile screen?
     path('m/<int:id>', views.movie, name = 'movie'),  # Movie profile screen
-
-    path("password-change/", auth_views.PasswordChangeView.as_view( # Password reset screen
-        template_name='registration/password-reset/change_password.html',
-        success_url = '/'),
-        name = "password_change"
-    ),
-    path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='registration/password-reset/password_reset_form.html',
-        subject_template_name='registration/password-reset/password_reset_subject.txt',
-        email_template_name='registration/password-reset/password_reset_email.html',
-        success_url='/accounts/login/'), name='password_reset'
-    ),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view( template_name='registration/password-reset/password_reset_done.html'),name='password_reset_done'),
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password-reset/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete//<uidb64>/<token>/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password-reset/password_reset_complete.html'), name='password_reset_complete'),
     path("welcome/", views.welcome, name = "send welcome email"),
-
     path('watch/', views.watch, name = 'watch'),
     path('friend/', views.friend, name = 'friend'),
-
     path('<str:username>/', views.profile, name='profile'),  # Profile screen
+    path('accounts/', include('django.contrib.auth.urls')),  # Enable Django auth app
 ]
+
 
 #### URL'S INCLUDED BY USING DJANGO AUTH ####################
 # accounts/login/ [name='login']
