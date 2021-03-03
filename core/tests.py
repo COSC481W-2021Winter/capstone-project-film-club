2cfa900e4fec81fd2ff321f18bead25c882a54f8
Supraja Kalva <suppikalva@gmail.com>
2021-03-02 20:36:10 -0500

    Updated the tests.py to resolve conflicts with main

--- core/tests.py
+++ core/tests.py
@@ -1,14 +1,16 @@
 from django.test import TestCase, Client, LiveServerTestCase
 from core.models import *
 from core.views import *
-import time 
+import time
 from django.urls import reverse
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
-# Create your tests here.
+
+# Model Test
+
 
 class GenreTestCase(TestCase):
     def setUp(self):
@@ -18,16 +20,16 @@ class GenreTestCase(TestCase):
         test = Genre.objects.get(api_id=3)
         self.assertEqual(test.__str__(), 'CO')
 
+
 # View Test
-
-
 class HomeTestCase(TestCase):
     def test_search_view(self):
-            client = Client()
-            response = client.get(reverse('core:home'))
-            response.status_code
+        client = Client()
+        response = client.get(reverse('core:home'))
+        response.status_code
 
-#Templete Test
+
+# Templete Test
 class GenresTestCase(LiveServerTestCase):
     def setUp(self):
         self.selenium = webdriver.Chrome()
@@ -67,10 +69,12 @@ class GenresTestCase(LiveServerTestCase):
         submit = selenium.find_element_by_class_name('btn')
 
         submit.send_keys(Keys.RETURN)
-        
+
         Url = self.live_server_url + '/genre'
 
         self.assertNotEquals(self.live_server_url, Url)
+
+
 class MoviePageTest(LiveServerTestCase):
     def setUp(self):
         self.selenium = webdriver.Chrome()
@@ -80,12 +84,11 @@ class MoviePageTest(LiveServerTestCase):
         super(MoviePageTest, self).setUp()
 
     def tearDown(self):
-
         self.selenium.quit()
 
         super(MoviePageTest, self).tearDown()
-    def signup(self):
 
+    def signup(self):
         selenium = self.selenium
         selenium.get(self.live_server_url + '/register')
 
@@ -104,13 +107,13 @@ class MoviePageTest(LiveServerTestCase):
         submit.send_keys(Keys.RETURN)
 
         assert 'favorite movie genre' in selenium.page_source
-    def test_home_page(self):
 
+    def test_home_page(self):
         selenium = self.selenium
-        selenium.get(self.live_server_url +'/m/252')
+        selenium.get(self.live_server_url + '/m/252')
         reviewbox = selenium.find_element_by_name("text")
         submitreview = selenium.find_element_by_name("Post")
-        
+
         assert "Willy Wonka" in selenium.page_source
         assert "Write A Review" in selenium.page_source
 
@@ -120,9 +123,118 @@ class MoviePageTest(LiveServerTestCase):
         assert 'Great movie! Gene Wilder is great!' in selenium.page_source
         markwatch = reviewbox = selenium.find_element_by_id("mark-watched")
         markwatch.send_keys(Keys.RETURN)
-        ##add markwatch detection
+        # add markwatch detection
+
+
+class HomePageProfileTestCase(LiveServerTestCase):
+    def setUp(self):
+        self.selenium = webdriver.Chrome()
+
+        self.signup()
+        self.pick_genres()
+
+        super(HomePageProfileTestCase, self).setUp()
+
+    def tearDown(self):
+        self.selenium.quit()
+
+        super(HomePageProfileTestCase, self).tearDown()
+
+    def signup(self):
+        selenium = self.selenium
+        selenium.get(self.live_server_url + '/register')
+
+        username = selenium.find_element_by_id('id_username')
+        email = selenium.find_element_by_id('id_email')
+        password1 = selenium.find_element_by_id('id_password1')
+        password2 = selenium.find_element_by_id('id_password2')
+
+        submit = selenium.find_element_by_name('register')
+
+        username.send_keys('Test123')
+        email.send_keys('test123@test.com')
+        password1.send_keys('homepagepass')
+        password2.send_keys('homepagepass')
+
+        submit.send_keys(Keys.RETURN)
+
+        self.user = User.objects.get(username='Test123')
+
+        assert 'favorite movie genre' in selenium.page_source
+
+    def pick_genres(self):
+        selenium = self.selenium
+
+        genre2 = Select(selenium.find_element_by_id('id_secondGenre'))
+        genre3 = Select(selenium.find_element_by_id('id_thirdGenre'))
+
+        submit = selenium.find_element_by_name('submit')
+
+        # genre2.select_by_index(2)
+        # genre3.select_by_index(3)
+
+        submit.send_keys(Keys.RETURN)
+
+        # print(selenium.page_source)
+
+        # assert 'Daily Recommendations' in selenium.page_source
+
+    def test_home_page(self):
+        selenium = self.selenium
+        selenium.get(self.live_server_url)
+
+        num_friends = selenium.find_element_by_id('num-friends')
+        num_movies = selenium.find_element_by_id('num-movies')
+        recently_watched = selenium.find_elements_by_class_name('rec-movie')
+
+        if len(recently_watched) == 0:
+            try:
+                no_recent = selenium.find_element_by_id('no-recent')
+            except:
+                assert len(recently_watched) > 0
+
+        assert len(num_friends.text) > 0
+        assert len(num_movies.text) > 0
+
+    def test_profile(self):
+        selenium = self.selenium
+
+        movie1 = Movie(title='Test Movie', description='literally not a real movie',
+                       api_id=1, poster_path='/image.png')
+        movie2 = Movie(title='Test Movie 2', description='literally not a real movie 2', api_id=2,
+                       poster_path='/image2.png')
+
+        movie1.save()
+        movie2.save()
+
+        review1 = Review(user=self.user, movie=movie1, title='Test Review',
+                         text='literally not a real review', score=1)
+        review2 = Review(user=self.user, movie=movie2, title='Test Review 2', text='literally not a real review 2',
+                         score=2)
+
+        review1.save()
+        review2.save()
+
+        selenium.get(self.live_server_url + '/Test123')
+
+        full_name = selenium.find_element_by_id('user-full-name').text
+        reviews = selenium.find_elements_by_class_name('review')
+
+        for review in reviews:
+            movie = Movie.objects.filter(
+                title=review.find_elements_by_class_name('media-title')[0].text)
+
+            assert movie.exists()
+
+            review = Review.objects.filter(user=self.user, movie=movie[0])
+
+            assert review.exists()
+
+        assert self.user.get_full_name() == full_name
 
 # Register Test
+
+
 class RegisterTestCase(LiveServerTestCase):
 
     def setUp(self):
@@ -148,7 +260,7 @@ class RegisterTestCase(LiveServerTestCase):
         assert 'Email' in selenium.page_source, "Email should be displayed"
         assert 'Password' in selenium.page_source, "Password should be displayed"
         assert 'Password confirmation' in selenium.page_source, "Password Confirm should be displayed"
-        assert 'Register' in selenium.page_source, "Register Button should be displayed" 
+        assert 'Register' in selenium.page_source, "Register Button should be displayed"
 
     # Verify if the functionality of register form works
 
@@ -296,7 +408,7 @@ class RegisterTestCase(LiveServerTestCase):
 
     # Verify that the validation of all the fields are as per business requirement
 
-    ## Test username
+    # Test username
     def test_username_register(self):
         selenium = self.selenium
         # Opening the link we want to test
@@ -308,7 +420,7 @@ class RegisterTestCase(LiveServerTestCase):
         selenium.find_element_by_id('id_email').send_keys('contact@yusuf.im')
 
         fake_usernames = ["$Smezy", "%!mezy",
-                          "^&*(Smezy)", "Smezy@.*", 
+                          "^&*(Smezy)", "Smezy@.*",
                           "s6SnGGAlkocT1Xr5wDyK2RoI986CCXFDhtngApHqN0Z8KWQ0S47XVgasjukjpEGKUN94obYeivFuy9mF3R5pThnHqY6mbdH9aZez9OGbH3jCjr3uoQVK8HjzhU5Nn7EYeDKUinvBck0mVvBTyAgy98KymhE",
                           "S_mezy"]
         for i in range((len(fake_usernames) - 1)):
@@ -319,7 +431,7 @@ class RegisterTestCase(LiveServerTestCase):
             selenium.find_element_by_id('id_register').send_keys(Keys.RETURN)
 
             time.sleep(1)
-            
+
             if (i < 4):
                 # assert "Enter a valid username. This value may contain only letters, numbers, and @/./+/-/_ characters." in selenium.page_source
                 assert "username" in selenium.page_source, "Should be on the same page."
@@ -327,7 +439,7 @@ class RegisterTestCase(LiveServerTestCase):
             else:
                 assert "favorite movie genres" in selenium.page_source, "Should be on the next page."
 
-    ## Test email
+    # Test email
     def test_email_register(self):
         selenium = self.selenium
         # Opening the link we want to test
@@ -357,7 +469,7 @@ class RegisterTestCase(LiveServerTestCase):
             else:
                 assert "favorite movie genres" in selenium.page_source, "Should be on the next page."
 
-    ## Test password
+    # Test password
     def test_password_register(self):
         selenium = self.selenium
         # Opening the link we want to test
@@ -370,10 +482,12 @@ class RegisterTestCase(LiveServerTestCase):
         selenium.find_element_by_id('id_email').send_keys('smile@game.org')
         fake_passwords = ["max", "deaf", "12345", "smile", "h$56sile4"]
         for i in range(len(fake_passwords)):
-            selenium.find_element_by_id('id_password1').send_keys(fake_passwords[i])
-            selenium.find_element_by_id('id_password2').send_keys(fake_passwords[i])
+            selenium.find_element_by_id(
+                'id_password1').send_keys(fake_passwords[i])
+            selenium.find_element_by_id(
+                'id_password2').send_keys(fake_passwords[i])
             selenium.find_element_by_id('id_register').click()
-            
+
             time.sleep(1)
 
             if (i < (len(fake_passwords) - 1)):
