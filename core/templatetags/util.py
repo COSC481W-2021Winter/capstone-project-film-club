977b43657b1f7624dc891eef445fb116c6fc34c7
Nick <nick.gilliam2@gmail.com>
2021-03-18 20:43:39 -0400

    added review colors and aggregate score breakdown

--- core/templatetags/util.py
+++ core/templatetags/util.py
@@ -4,6 +4,8 @@ import arrow
 
 register = template.Library()
 
+colors = [('#41a749', 5), ('#70b73a', 4), ('#d8d21c', 3), ('#d27635', 2), ('#a74141', 1), ('#a74141', 0)]
+
 @register.simple_tag
 def is_friend(user, profile):
     return user.userprofile.friends.filter(username = profile.username).exists()
@@ -14,3 +16,16 @@ def get_normal_time(datetime):
         return 'None'
 
     return arrow.get(datetime.replace('T', ' ').replace('Z', ' ').split('.')[0], 'YYYY-M-D HH:mm:ss').humanize()
+
+@register.simple_tag
+def get_review_color(score):
+    if score == '' or score is None:
+        return ''
+
+    score = int(score)
+
+    for color_pair in colors:
+        if score >= color_pair[1]:
+            return color_pair[0]
+
+    return color_pair[2][0]
