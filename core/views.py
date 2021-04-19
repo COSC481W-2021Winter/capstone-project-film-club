66319a6f25e72cae2806f1b0548b9a7bf2efe3a9
nfishel <n-fishel_v@dcunet.org>
2021-04-19 15:49:38 -0400

    make all 4 recs load

--- core/views.py
+++ core/views.py
@@ -613,16 +613,15 @@ def get_recommendations(user):
     if len(genres) == 0:
         return recommendations
 
+    recommendations_list = []
+
     for x in range(num_recommendations): 
         genre = genres[x % len(genres)].api_id
 
         response = requests.get('https://api.themoviedb.org/3/discover/movie?api_key=a1a486ad19b99d238e92778b9ceb4bb4&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=' + str(random.randint(1,100)) +'&with_genres=' + str(genre))
         results = response.json()['results']
 
-        #print(random.randrange(20, 50, 3))
-        selection_index = random.randrange(0, len(results))
-
-        index = selection_index
+        index = 0
 
         while index < 5:
             if index >= len(results):
@@ -635,8 +634,6 @@ def get_recommendations(user):
                 break
 
             index += 1
-
-    #recommendations = []
 
     return recommendations
 def get_similar(id):
